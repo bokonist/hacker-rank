8421d440be8006b7aad8e4aca0350f34e590aa12
bokonist <jayrm1995@gmail.com>
2016-11-23 19:43:32 +0530

    standard deviation solved

---
+++ 10-days-of-statistics/standard-deviation.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+/* useful utility macros */
+#define MIN(a,b,res) ((a)>(b))? (res)=(b):(res)=(a)
+#define MAX(a,b,res) ((a)<(b))? (res)=(b):(res)=(a)
+#define printArray(a,l,n) for(int(i)=(l);(i)<int(n);i++) { cout<<(a[i])<<' ';}
+#define printWholeArray(a) for(int(i)=0;(i)<int((sizeof(a)/sizeof(a[0])));i++) { cout<<(a[i])<<' ';}
+#define repeat(n) for(int (i)=0;(i)<(int)(n);++(i)) //repeat n times
+#define repeatBounds(l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i)) //repeat lower to upperbound
+#define until(x,val) for(int(i) = int(x); int(x)!=int(val); (i<val)?x++:x--) //until x becomes val through increment or decrement
+
+/*some constants*/
+static const long double PI = 3.142857143;
+
+/*some shorthands*/
+typedef long long ll;
+#define op cout<< // output to stdout
+#define ip cin>> //input from stdin
+#define err cerr<< //output to stderr
+#define nl cout<<"\n"; //newline
+
+int main()
+{
+	int n,temp;
+	float mean=0;
+	ip n;
+	vector<int> v;
+	repeat(n)
+	{
+		ip temp;
+		v.push_back(temp);
+		mean +=temp;
+	}
+	mean/=(float)n;
+	repeat(n)
+	{
+
+	}
+	return 0;
+}
