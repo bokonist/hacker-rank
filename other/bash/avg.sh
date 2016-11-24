67ba9c0e258bb632f2f703abe797c11bcc34b04f
bokonist <jayrm1995@gmail.com>
2016-11-25 02:42:31 +0530

    testing

--- other/bash/avg.sh
+++ other/bash/avg.sh
@@ -1,8 +1,8 @@
 #https://www.hackerrank.com/challenges/bash-tutorials---compute-the-average
 #!/bin/bash    
 sum=0
-read n
-while read -r line || [[ -n "$line" ]]; do
+read n #number of inputs
+while read -r line || [[ -n "$line" ]]; do #while a line is read and it is not EOF
     sum=$(($sum + $line))
 done
-printf "%.3f" $(echo "scale=10; $sum/$n" | bc -l)
+printf "%.3f" $(echo "scale=10; $sum/$n" | bc -l) #use bc to handle floating points in bash
