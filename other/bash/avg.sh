ad03c0d07a1e286dc007a0029112810faf800664
bokonist <jayrm1995@gmail.com>
2016-11-24 22:01:13 +0530

    solved bash avg

---
+++ other/bash/avg.sh
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/bash-tutorials---compute-the-average
+#!/bin/bash    
+sum=0
+read n
+while read -r line || [[ -n "$line" ]]; do
+    sum=$(($sum + $line))
+done
+printf "%.3f" $(echo "scale=10; $sum/$n" | bc -l)
