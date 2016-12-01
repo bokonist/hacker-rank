a32d984800580b47a9dc7f2bdc816564f75212f5
bokonist <jayrm1995@gmail.com>
2016-12-02 04:06:46 +0530

    solved 1

---
+++ week-of-code-26/army-game.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+using namespace std;
+/* useful utility macros */
+#define MIN(a,b,res) ((a)>(b))? (res)=(b):(res)=(a)
+#define MAX(a,b,res) ((a)<(b))? (res)=(b):(res)=(a)
+#define printArray(a,l,n) for(int(i)=(l);(i)<int(n);i++) { cout<<(a[i])<<' ';}
+#define printWholeArray(a) for(int(i)=0;(i)<int((sizeof(a)/sizeof(a[0])));i++) { cout<<(a[i])<<' ';}
+#define repeat(n) for(int (i)=0;(i)<(int)(n);++(i)) //repeat n times
+#define repeatBounds(l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i)) //repeat lower to upperbound
+#define until(x,val) for(int(i) = int(x); int(x)!=int(val); (i<val)?x++:x--) //until x becomes val through increment or decrement
+
+/*some constants*/
+static const long double PI = 3.142857143;
+
+/*some shorthands*/
+typedef long long ll;
+#define op cout<< // output to stdout
+#define ip cin>> //input from stdin
+#define err cerr<< //output to stderr
+#define nl cout<<"\n"; //newline
+
+int main()
+{
+	int t;
+	ip t;
+	while(t--)
+	{
+		int noOfDrops=0;
+		int n,m;
+		ip n>>m;
+		if(n&1 && m&1)
+			noOfDrops++;
+		noOfDrops+= (n/2)*(m/2) + (n%2)*(m/2) + (m%2)*(n/2);
+		op noOfDrops; nl
+	}
+	return 0;
+}
