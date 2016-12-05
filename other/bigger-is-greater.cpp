80925789c4b7b8163780f774ef2e497139f3418d
bokonist <jayrm1995@gmail.com>
2016-12-05 11:33:29 +0530

    solved

---
+++ other/bigger-is-greater.cpp
@@ -0,0 +1,41 @@
+// https://www.hackerrank.com/challenges/bigger-is-greater
+/* author: github.com/bokonist */
+#include "bits/stdc++.h"
+using namespace std;
+/* useful utility macros */
+#define MIN(a,b,res) ((a)>(b))? (res)=(b):(res)=(a)
+#define MAX(a,b,res) ((a)<(b))? (res)=(b):(res)=(a)
+#define printArray(a,l,n) for(int(i)=(l);(i)<int(n);i++) { cout<<(a[i])<<' ';}
+#define printWholeArray(a) for(int(i)=0;(i)<int((sizeof(a)/sizeof(a[0])));i++) { cout<<(a[i])<<' ';}
+#define repeat(n) for(int (i)=0;(i)<(int)(n);++(i)) //repeat n times
+#define repeatBounds(l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i)) //repeat lower to upperbound
+#define until(x,val) for(int(i) = int(x); int(x)!=int(val); (i<val)?x++:x--) //until x becomes val through increment or decrement
+
+/*some constants*/
+static const long double PI = 3.142857143;
+
+/*some shorthands*/
+typedef long long ll;
+#define op cout<< // output to stdout
+#define ip cin>> //input from stdin
+#define err cerr<< //output to stderr
+#define nl cout<<"\n"; //newline
+
+int main()
+{
+	ll t;
+	ip t;
+	while(t--)
+	{
+		string s;
+		ip s;
+		int k=1;
+		k= next_permutation(s.begin(),s.end());
+		if(k==0)
+			op "no answer";
+		else
+			op s;
+		nl;
+	}
+	return 0;
+}
